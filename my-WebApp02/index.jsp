6cb71638997ea84dcea31e99bc177e38ba8e3679
yangzhanghong <3253561985qq.com>
2019-03-21 09:48:01 +0800

    my-WebApp02项目上传

---
+++ my-WebApp02/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<%@ page language="java" pageEncoding="gb2312"%>
+<body>
+<h2>杨章洪</h2>
+</body>
+</html>
---
+++ my-web/src/my_class/HelloWorld.java
@@ -0,0 +1,9 @@
+package my_class;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+	}
+
+}
